9da213aeeeef106378ba7893147f8072e7619e70
adlan-prog <-adlan14@gmail.com adlan-prog>
2023-06-17 11:59:06 +0300

    asd

---
+++ my_first_refactor/cypress/integration/signup.spec.js
@@ -0,0 +1,86 @@
+///<reference types="cypress" /> 
+Cypress.on('uncaught:exception', () => false);
+
+import meUser from '/cypress/fixtures/me-user.json';
+import {getRandomNumber} from '/cypress/support/utils';
+
+function loginMe() {
+
+        // cy.visit('/'); we have beforeEach
+
+        cy.get('.navbar').should('be.visible').as('appHeader');
+        cy.get('@appHeader').find('[href$="/login"]').click();
+        cy.url().should('include', '/#/login');
+
+        cy.get('.auth-page').should('be.visible').as('loginPage');
+        cy.get('@loginPage').find('h1').should('have.text', 'Sign in');
+        cy.get('.auth-page form').should('be.visible').as('loginForm');
+
+        cy.get('@loginForm').find('input[ng-model$=email]').type(meUser.email);
+        cy.get('@loginForm').find('input[ng-model$=password]').type(meUser.password);
+        cy.get('@loginForm').find ('button[type=submit]').click();
+
+        cy.get('@appHeader').should('contain.text', meUser.username);
+
+    
+
+}
+
+describe('Sign up', () => {
+
+    // will be executed before each it()
+    beforeEach(() => {
+        cy.visit('/');
+    });
+
+    it('should do register user', () => {
+
+        // cy.visit('/');  we have beforeEach
+        cy.get('.navbar').should('be.visible').as('appHeader');
+
+        cy.get('@appHeader').find('a[href$="/register"]').click();
+        cy.url().should('include', '/#/register');
+
+        cy.get('.auth-page').as('registerPage');
+        cy.get('@registerPage').find('h1').should('have.text', 'Sign up');
+        cy.get('@registerPage').find('form').should('be.visible').as('registerForm');
+
+        const rnd = getRandomNumber(1000, 9999);
+        const username = 'user_' + rnd;
+        const email = username + '@gmail.com';
+
+
+        cy.get('@registerForm').find('input[ng-model$=username]').type(username);
+        cy.get('@registerForm').find('input[ng-model$=email]').type(email);
+        cy.get('@registerForm').find('input[ng-model$=password]').type('xyzXYZ123_');
+        cy.get('@registerForm').find('button[type=submit]').click();
+
+        cy.get('@appHeader').should('contain.text', username);
+
+    });
+
+    it('should do login user', () => {
+
+        loginMe();
+
+    });
+
+    it('should do logout user', () => {
+
+        // cy.visit('/'); we have beforeEach
+
+        loginMe();
+
+        // for logout
+        cy.get('@appHeader').find ('a[href$="/settings"]').click();
+        cy.url().should('include', '/#/settings');
+        cy.get('.settings-page').as('settingsPage');
+
+        cy.get('@settingsPage').find('h1').should('have.text', 'Your Settings');
+        // TODO: improve selector to button[data-cy=logout]
+        cy.get('@settingsPage').find('button[ng-click*=logout]').click();
+
+        cy.get('@appHeader').should('not.contain.text', meUser.username);
+    });
+
+});
---
+++ my_first_refactor/js_examples/say_hello.js
@@ -0,0 +1,10 @@
+function sayHelloFor(firstName, lastName) {
+    const fullName = lastName + ' ' + firstName;
+    console.log('Hello', fullName);
+    console.log('Today is', new Date());
+}
+
+sayHelloFor('Bob', 'Marley');
+sayHelloFor('Bill', 'Gates');
+sayHelloFor('Tim', 'Cook');
+sayHelloFor('adlan', 'solt');
